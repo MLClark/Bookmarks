6261aa16e450f52187fb5df6bd019ad229fcfdf6
mclark <mclark@peoplestrategy.com>
2021-08-20 10:35:27 -0500

    EHCM-34856 commit all deletes and mods

--- REPORTING/BUG FIXES/EHCM-34853/research-ehcm-34853.sql
+++ REPORTING/BUG FIXES/EHCM-34853/research-ehcm-34853.sql
@@ -1,3 +1,10 @@
-select * from cognos_pspaypayrollregistertaxes_winter_mv where personid = '13893' and etv_id = 'T02' and check_date 
-select * from person_names where lname like 'Connor';
-;
+
+select * from person_names where lname like 'Autry' and current_date between effectivedate and enddate and current_timestamp between createts and endts and nametype = 'Legal';
+select * from person_names where lname like 'Bias' and current_date between effectivedate and enddate and current_timestamp between createts and endts and nametype = 'Legal';
+select * from person_names where lname like 'Howard' and current_date between effectivedate and enddate and current_timestamp between createts and endts and nametype = 'Legal';
+select * from person_names where lname like 'Maw' and current_date between effectivedate and enddate and current_timestamp between createts and endts and nametype = 'Legal';
+select * from person_names where lname like 'Wright' and current_date between effectivedate and enddate and current_timestamp between createts and endts and nametype = 'Legal';
+
+
+select * from cognos_pspaypayrollregistertaxes_winter_mv where personid in ('13609', '13638', '10309', '10662', '10660') 
+   and etv_id = 'T02' and check_date = '2021-08-05';
