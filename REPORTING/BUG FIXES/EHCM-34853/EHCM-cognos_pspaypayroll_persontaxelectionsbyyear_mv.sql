280add07ffadaa64dd0615feff5befa6677c27a7
mclark <mclark@peoplestrategy.com>
2021-08-20 10:31:29 -0500

    EHCM-34853 add sql files

---
+++ REPORTING/BUG FIXES/EHCM-34853/EHCM-cognos_pspaypayroll_persontaxelectionsbyyear_mv.sql
@@ -0,0 +1,21 @@
+--CREATE MATERIALIZED VIEW public.cognos_pspaypayroll_persontaxelectionsbyyear_mv TABLESPACE pg_default AS 
+SELECT DISTINCT date_part('year'::text, rpgd.periodpaydate) AS tlayear,
+    taxrank.ttype_tax_code,
+    taxrank.etvid_lookups,
+    pte.taxid,
+    pte.personid,
+    taxrank.taxrank
+   FROM runpayrollgetdates rpgd
+     JOIN person_tax_elections pte ON ("overlaps"(rpgd.periodstartdate::timestamp with time zone, rpgd.periodenddate::timestamp with time zone, pte.effectivedate::timestamp with time zone, pte.enddate::timestamp with time zone) OR pte.effectivedate = rpgd.periodenddate) AND now() >= pte.createts AND now() <= pte.endts AND pte.effectivedate < pte.enddate
+     JOIN ( SELECT ta.ttype_tax_code,
+            ta.etvid_lookups,
+            ta.taxid,
+            pte_1.personid,
+            max(pte_1.effectivedate) AS effectivedate,
+            max(pte_1.createts) AS createts,
+            rank() OVER (PARTITION BY pte_1.personid, ta.ttype_tax_code, ta.taxid ORDER BY (max(pte_1.effectivedate)) DESC) AS taxrank
+           FROM tax_lookup_aggregators ta
+             JOIN person_tax_elections pte_1 ON pte_1.taxid = ta.taxid
+          WHERE pte_1.effectivedate < pte_1.enddate AND now() >= pte_1.createts AND now() <= pte_1.endts
+          GROUP BY ta.ttype_tax_code, ta.etvid_lookups, ta.taxid, pte_1.personid) taxrank ON taxrank.taxrank = 1 AND taxrank.personid = pte.personid AND taxrank.taxid = pte.taxid AND pte.effectivedate = taxrank.effectivedate
+--WITH NO DATA;
